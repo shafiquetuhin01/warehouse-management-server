41d89ed830d6e06657c24264d89a448ec6bcb07b
Shafique <sfqtuhin04@gmail.com>
2022-05-07 00:17:58 +0600

    mongodb updated

--- index.js
+++ index.js
@@ -1,15 +1,47 @@
-const express = require('express');
-const app = express();
-const cors = require('cors');
+const express = require("express");
+require('dotenv').config();
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
+const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res)=>{
-    res.send("My code is running for the first time");
+//user: shafiqueFirmHouse
+//pass: MnWfRCCvBvocPinD
+
+// const uri = "mongodb+srv://shafiqueFirmHouse :MnWfRCCvBvocPinD@cluster0.opjrt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.opjrt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
 });
 
-app.listen(port, ()=>{
-    console.log('My code is listening as', port);
+async function run(){
+    try{
+        await client.connect();
+        const myCollection = client.db('firmHouse').collection('allAnimals');
+        app.get('/service',async(req, res)=>{
+            const query = {};
+            const cursor = myCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+    }
+    finally{
+
+    }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("My code is running for the first time");
+});
+
+app.listen(port, () => {
+  console.log("My code is listening as", port);
 });
