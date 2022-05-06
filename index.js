ec53771ab322b5deb227a1b253ed191e696bc45c
Shafique <sfqtuhin04@gmail.com>
2022-05-06 19:35:52 +0600

    server site updating

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res)=>{
     res.send("My code is running for the first time");
